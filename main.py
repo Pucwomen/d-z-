34a7b8eb725dbdb9a65937ac929ee9de633ead4b
Pucwomen <qwertzxc345456@gmail.com>
2024-10-23 07:37:50 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+my_string=input("введите строку: ")
+my_string=("hello? world:")
+print(len(my_string))
+print("my_string".upper())
+print("my_string".lower())
+my_string[:1]
+my_string[2:]
+
+
+
+
